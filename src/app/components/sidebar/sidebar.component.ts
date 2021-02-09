d2b6b343cf573b862921f87074bef6e683dcdb11
Ravi-FEHCJ-Developer! <ravi96it@gmail.com>
2021-02-09 07:15:29 +0530

    rv

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,20 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 declare const $: any;
-declare interface RouteInfo {
+
+declare interface RouteInfo 
+{
     path: string;
     title: string;
     icon: string;
     class: string;
 }
-export const ROUTES: RouteInfo[] = [
+export const ROUTES: RouteInfo[] = 
+[
   { path: '/master', title: 'Master',  icon:'person', class: '' },
   { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
-    { path: '/table-list', title: 'Table List',  icon:'content_paste', class: '' },
-    { path: '/typography', title: 'HRh',  icon:'library_books', class: '' },
-    { path: '/icons', title: 'Icons',  icon:'bubble_chart', class: '' },
-    { path: '/maps', title: 'Maps',  icon:'location_on', class: '' },
-    { path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' }
+  { path: '/table-list', title: 'Table List',  icon:'content_paste', class: '' },
+  { path: '/typography', title: 'HRh',  icon:'library_books', class: '' },
+  { path: '/icons', title: 'Icons',  icon:'bubble_chart', class: '' },
+  { path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' }
 ];
 
 @Component({
--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -5,7 +5,6 @@ import { UserProfileComponent } from '../../user-profile/user-profile.component'
 import { TableListComponent } from '../../table-list/table-list.component';
 import { TypographyComponent } from '../../typography/typography.component';
 import { IconsComponent } from '../../icons/icons.component';
-import { MapsComponent } from '../../maps/maps.component';
 import { NotificationsComponent } from '../../notifications/notifications.component';
 import { UpgradeComponent } from '../../upgrade/upgrade.component';
 
@@ -33,13 +32,7 @@ export const AdminLayoutRoutes: Routes = [
     //         path: 'notifications',
     //         component: NotificationsComponent
     //     }]
-    // }, {
-    //     path: '',
-    //     children: [ {
-    //         path: 'maps',
-    //         component: MapsComponent
-    //     }]
-    // }, {
+    // },{
     //     path: '',
     //     children: [ {
     //         path: 'typography',
@@ -57,7 +50,6 @@ export const AdminLayoutRoutes: Routes = [
     { path: 'table-list',     component: TableListComponent },
     { path: 'typography',     component: TypographyComponent },
     { path: 'icons',          component: IconsComponent },
-    { path: 'maps',           component: MapsComponent },
     { path: 'notifications',  component: NotificationsComponent },
     { path: 'upgrade',        component: UpgradeComponent },
 ];
