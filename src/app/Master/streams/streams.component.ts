0cd0598254dc544dcea6b97a868295a3b2c748ca
Harsh <harsh94664@gmail.com>
2021-02-10 12:39:29 +0530

    Harsh

--- src/app/Master/streams/streams.component.ts
+++ src/app/Master/streams/streams.component.ts
@@ -14,6 +14,7 @@ Stream:Streams;
   constructor(private streamsService:StreamsService) { }
 
   ngOnInit(): void {
+    this.getStreams();
   }
 
   getStreams(){
--- src/app/Master/type-details/type-details.component.ts
+++ src/app/Master/type-details/type-details.component.ts
@@ -14,6 +14,7 @@ TypeDetail:TypeDetails;
   constructor(private typeDetailService:TypeDetailsService) { }
 
   ngOnInit(): void {
+    this.getTypeDetails();
   }
 
   getTypeDetails(){
