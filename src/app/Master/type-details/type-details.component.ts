0bdd9e8ceb948cb87862c8317eef2233ad4d557b
Harsh <harsh94664@gmail.com>
2021-02-10 11:22:15 +0530

    H

--- src/app/Master/type-details/type-details.component.ts
+++ src/app/Master/type-details/type-details.component.ts
@@ -1,3 +1,5 @@
+import { TypeDetailsService } from './../../Services/Type-Details/type-details.service';
+import { TypeDetails } from './../../Interface/type-details';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./type-details.component.css']
 })
 export class TypeDetailsComponent implements OnInit {
+TypeDetails:TypeDetails[];
+TypeDetail:TypeDetails;
 
-  constructor() { }
+  constructor(private typeDetailService:TypeDetailsService) { }
 
   ngOnInit(): void {
+  }
+
+  getTypeDetails(){
+    this.typeDetailService.getMasterTypeDetails().subscribe
+    (
+      (res) =>{
+        this.TypeDetails=res; 
+        console.log(res);
+      },
+      (error)=>{
+        console.log("Error in Get Type Details !");
+    })
+  }
+
+  getTypeDetail(id:number){
+    this.typeDetailService.getMasterTypeDetailsById(id).subscribe
+    (
+      (res) =>{ 
+        console.log(res);
+      },
+      (error)=>{
+        console.log("Error in Get Type Details !");
+    })
+  }
+
+  insertTypeDetails(data:TypeDetails){
+    this.typeDetailService.insertMasterTypeDetails(data).subscribe
+    (
+      (res) =>{ 
+        console.log(res);
+      },
+      (error)=>{
+        console.log("Error in Get Stream By Id !");
+    }
+    )
   }
 
 }
--- src/app/Master/type/type.component.ts
+++ src/app/Master/type/type.component.ts
@@ -1,3 +1,5 @@
+import { TypeService } from './../../Services/Type/type.service';
+import { Type } from './../../Interface/type';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./type.component.css']
 })
 export class TypeComponent implements OnInit {
-
-  constructor() { }
+Types:Type[];
+Type:Type;
+  constructor(private typeService:TypeService) { }
 
   ngOnInit(): void {
+  }
+
+  getTypes(){
+    this.typeService.getMasterType().subscribe
+    (
+      (res) =>{ 
+        this.Types=res;
+        console.log(res);
+      },
+      (error)=>{
+        console.log("Error in Get Master Type!");
+    })
+  }
+
+  getType(id:number){
+    this.typeService.getMasterTypeById(id).subscribe
+    (
+      (res) =>{ 
+        this.Type=res;
+        console.log(res);
+      },
+      (error)=>{
+        console.log("Error in Get Type By Id!");
+    })
+  }
+
+  insertType(data:Type){
+    this.typeService.insertMasterType(data).subscribe
+    (
+      (res) =>{ 
+        console.log(res);
+      },
+      (error)=>{
+        console.log("Error in Post Type!");
+    })
+  }
+
+  updateType(id:number,data:Type){
+    this.typeService.updateMasterType(id,data).subscribe
+    (
+      (res) =>{ 
+        console.log(res);
+      },
+      (error)=>{
+        console.log("Error in Update Type!");
+    })
+  }
+
+  deleteType(id:number){
+    this.typeService.deleteMasterType(id).subscribe
+    (
+      (res) =>{ 
+        console.log(res);
+      },
+      (error)=>{
+        console.log("Error in Delete Type!");
+    })
   }
 
 }
